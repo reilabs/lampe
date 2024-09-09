04ad5a1464263df17390516186bb17662bc5663d
Marcin Kostrzewa <marckostrzewa@gmail.com>
2024-09-09 22:26:58 +0200

    function def syntax

--- Lampe/Lampe/Syntax.lean
+++ Lampe/Lampe/Syntax.lean
@@ -15,15 +15,12 @@ declare_syntax_cat nr_block_contents
 syntax ident:nr_ident
 syntax ident"::"nr_ident : nr_ident
 
-
-
 partial def elabNrIdent : Syntax → MetaM String
 | `(nr_ident|$x:ident) => pure $ x.getId.toString
 | `(nr_ident|$x:ident::$y:nr_ident) => do
   let tl ← elabNrIdent y
   pure $ s!"{x.getId.toString}::{tl}"
 | _ => throwUnsupportedSyntax
-
 
 syntax ident:nr_type
 syntax ident "<" nr_type,* ">" : nr_type
@@ -83,51 +80,30 @@ partial def elabExpr (rep : Q(Tp → Type)) (varMap : String → Option ((tp : Q
 
 end
 
-
 declare_syntax_cat nr_param_decl
 syntax ident ":" nr_type : nr_param_decl
 syntax nr_fn_decl := "fn" nr_ident "<" ident,* ">" "(" nr_param_decl,* ")" "->" nr_type "{" sepBy(nr_expr, ";", ";", allowTrailingSep) "}"
 
-def tearOffHNil (emb : β → Type) (a : α) : HList emb [] → α := fun h![] => a
-
-def tearOffHNilQ (α β : Q(Type)) (emb : Q($β → Type)) (a : Q($α)) : Q(HList $emb [] → $α) :=
-  q(fun h![] => $a)
-
-@[reducible]
-def tearOffHLCons {a : α} {as : List α} {emb : α → Type} (f : emb a → HList emb as → β) : HList emb (a :: as) → β
-| HList.cons x xs => f x xs
-
-def tearOffHLConsQ {α β: Q(Type)} {a : Q($α)} {as : Q(List $α)} {emb : Q($α → Type)} (f : Q($emb $a → HList $emb $as → $β))
-  : Q(HList $emb ($a :: $as) → $β) := q(tearOffHLCons $f)
-
-def elabFnOfGenerics {β : Q(Type)} (α: Q(Type))
-    (emb : Q($α → Type))
-    (generics: Q(List $α))
-    (idents : List Ident)
-    (bodyGen : (Ident → Option Lean.Expr) -> MetaM Q($α))
-    : MetaM Q(HList $emb $generics → $β)
-  := match idents with
-  | [] => do
-    let b ← bodyGen fun _ => .none
-    pure $ tearOffHNilQ β _ q(Kind.denote) b
-  | id :: ids => do
-    let lam ← withLocalDeclDQ (Name.mkSimple id) q(Kind.denote .type) fun v => do
-      let body ← elabFnOfGenerics α emb generics ids fun ids => do
-        bodyGen fun i => if i = id then some v else ids i
-      mkLambdaFVars #[v] body
-    dbg_trace lam
-    mkAppM ``tearOffHLCons #[lam]
-    -- pure $ tearOffHLConsQ (emb := q(Kind.denote)) lam
-
 def mkLambda (pName : Ident) (pType : Lean.Expr) (body : Lean.Expr → MetaM Lean.Expr): MetaM Lean.Expr := do
   withLocalDeclD (Name.mkSimple pName) pType fun p => mkLambdaFVars #[p] =<< body p
 
-def mkLambdaOfHList (base : List (Ident × Lean.Expr)) (emb : Lean.Expr) (body : (Ident → Option Lean.Expr) → MetaM Lean.Expr) : MetaM Lean.Expr := match base with
-| [] => do mkAppM ``tearOffHNil #[emb, ←body fun _ => none]
-| (id, tp) :: rest => do
-  let int ← mkLambda id (←mkAppM' emb #[tp]) fun h =>
-    mkLambdaOfHList rest emb fun vmap => body (fun i => if i = id then some h else vmap i)
-  mkAppM ``tearOffHLCons #[int]
+def HList.head : HList emb (a :: as) → emb a
+| HList.cons x _ => x
+
+def HList.tail : HList emb (a :: as) → HList emb as
+| HList.cons _ xs => xs
+
+def mkHListDestruct (base : List (Ident × Lean.Expr)) (emb : Lean.Expr) (hlist : Lean.Expr) (body : (Ident → Option (Lean.Expr × Lean.Expr)) → MetaM Lean.Expr) : MetaM Lean.Expr := match base with
+| [] => body fun _ => .none
+| (id, e) :: rest => do
+  withLetDecl (Name.mkSimple id) (←mkAppM' emb #[e]) (←mkAppM ``HList.head #[hlist]) fun v => do
+    let rest ← mkHListDestruct rest emb (←mkAppM ``HList.tail #[hlist]) fun vmap => body (fun i => if i = id then some (e, v) else vmap i)
+    mkLetFVars #[v] rest
+
+def mkLambdaOfHList' (pName: Ident) (elemType : Lean.Expr) (base : List (Ident × Lean.Expr)) (emb : Lean.Expr) (body : (Ident → Option (Lean.Expr × Lean.Expr)) → MetaM Lean.Expr) :
+    MetaM Lean.Expr := do
+  let args ← mkListLit elemType (base.map (·.2))
+  mkLambda pName (←mkAppM ``HList #[emb, args]) fun h => mkHListDestruct base emb h body
 
 def elabParams (varMap : (Ident  → Option Q(Tp))) : Array Syntax → MetaM (List (Ident × Q(Tp))) := fun params => do
   params.toList.mapM fun p => match p with
@@ -139,24 +115,24 @@ def elabList (α : Q(Type)) : List Q($α) → Q(List $α)
 | x :: xs => let tail := elabList α xs; q($x :: $tail)
 
 def elabFnDecl : Syntax → MetaM Lean.Expr
-| `(nr_fn_decl| fn $name < $generics,* > ( $params,* ) -> $outTp { $body }) => do
+| `(nr_fn_decl| fn $name < $generics,* > ( $params,* ) -> $outTp { $bExprs;* }) => do
   let name : Q(Ident) ← mkStrLit <$> elabNrIdent name
   let generics := generics.getElems.toList.map fun i => (i.getId.toString, mkConst ``Kind.type)
   let genericsDecl ← mkListLit q(Kind) (generics.map (·.2))
-  let paramTys ← mkLambdaOfHList generics (mkConst ``Kind.denote) fun varMap => do
-    let params ← elabParams varMap params.getElems
+  let paramTys ← mkLambdaOfHList' "generics" (mkConst ``Kind) generics (mkConst ``Kind.denote) fun varMap => do
+    let params ← elabParams (fun i => Prod.snd <$> varMap i) params.getElems
     mkListLit q(Tp) (params.map Prod.snd)
-  let out ← mkLambdaOfHList generics (mkConst ``Kind.denote) fun varMap => do
-    elabNrType varMap outTp
+  dbg_trace paramTys
+  let out ← mkLambdaOfHList' "generics" (mkConst ``Kind) generics (mkConst ``Kind.denote) fun varMap => do
+    elabNrType (fun i => Prod.snd <$> varMap i) outTp
   let body ← mkLambda "rep" q(Tp → Type) fun rep => do
-    mkLambdaOfHList generics (mkConst ``Kind.denote) fun tyVars => do
-      let params ← elabParams tyVars params.getElems
-      mkLambdaOfHList (params.map Prod.snd)
-
-  let fn ← mkAppM ``Function.mk #[genericsDecl, paramTys, out]
-  -- pure params
+    mkLambdaOfHList' "generics" (mkConst ``Kind) generics (mkConst ``Kind.denote) fun tyVars => do
+      let params ← elabParams (fun i => Prod.snd <$> tyVars i) params.getElems
+      mkLambdaOfHList' "arguments" (mkConst ``Tp) params rep fun varMap => do
+        let ⟨_, expr⟩ ← elabBlock rep (fun i => (fun (tp, v) => ⟨tp, v⟩) <$> varMap i) bExprs.getElems.toList
+        pure expr
+  let fn ← mkAppM ``FunctionDecl.mk #[name, ←mkAppM ``Function.mk #[genericsDecl, paramTys, out, body]]
   pure fn
-  -- pure q(FunctionDecl.mk $name (Function.mk $genericsDecl $params $out sorry))
 | _ => throwUnsupportedSyntax
 
 elab "expr![" expr:nr_expr "]" : term => do pure (← elabExpr q(Tp.denote 17) (fun _ => none) expr).2
@@ -165,7 +141,7 @@ elab "nrfn![" fn:nr_fn_decl "]" : term => do (elabFnDecl fn)
 #check expr![ (1 : u8)]
 #check expr![ { let y = (1 : u8) ; let y = y ; y} ]
 
-#check nrfn![ fn myFn<A, B, C>(x : u8, y : A, z : B, w : C) -> u8 { x }]
+#check nrfn![ fn myFn<A, B, C>(x : u8, y : A, z : B, w : C) -> u8 { let hehe = x ; hehe } ]
 
 
 end Lampe
