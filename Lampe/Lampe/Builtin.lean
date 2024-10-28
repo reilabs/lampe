6ae57fc4cee98834b5a8e0a27e39c8baba5f61d7
utkn <utkns09@gmail.com>
2024-10-28 13:39:12 +0300

    Doc comments

--- Lampe/Lampe/Builtin.lean
+++ Lampe/Lampe/Builtin.lean
@@ -161,7 +161,16 @@ inductive builtinOmni
       → Q none
       → (builtinOmni argTps outTp pred comp) p st argTps outTp args Q
 
--- Generic Builtin definition constructor
+/--
+A `Builtin` definition.
+Takes a list of input types `argTps : List Tp`, an output type `outTp : Tp`, a predicate `pred` and evaluation function `comp`.
+For `args: HList (Tp.denote p) argTps`, we assume that a builtin function *fails* when `¬(pred args)`, and it *succeeds* when `pred args`.
+
+If the builtin succeeds, it evaluates to `some (comp args (h : pred))`.
+Otherwise, it evaluates to `none`.
+```
+```
+-/
 def newBuiltin
   (argTps : List Tp)
   (outTp : Tp)
@@ -183,36 +192,67 @@ def newBuiltin
     . apply builtinOmni.err <;> assumption
 }
 
--- a + b
--- Assumption: Integer overflow throws a runtime exception.
+/--
+For `s ∈ ℕ`, defines the addition of two `s`-bit uints: `(a b: Tp.denote (Tp.u s))`.
+We make the following assumptions:
+- If `(a + b) < 2^s`, then the builtin returns `(a + b) : Tp.denote (Tp.u s)`
+- Else (integer overflow), an exception is thrown.
+
+In Noir, this builtin corresponds to `a + b` for unsigned integers `a`, `b` of bit-length `s`.
+-/
 def uAdd {s} := newBuiltin
   [(.u s), (.u s)] (.u s)
   (fun h![a, b] => (a + b) < 2^s)
   (fun h![a, b] _ => a + b)
 
--- a * b
--- Assumption: Integer overflow throws a runtime exception.
+/--
+For `s ∈ ℕ`, defines the multiplication of two `s`-bit uints: `(a b: Tp.denote (Tp.u s))`.
+We make the following assumptions:
+- If `(a * b) < 2^s`, then the builtin returns `(a * b) : Tp.denote (Tp.u s)`
+- Else (integer overflow), an exception is thrown.
+
+In Noir, this builtin corresponds to `a * b` for unsigned integers `a`, `b` of bit-length `s`.
+-/
 def uMul {s} := newBuiltin
   [(.u s), (.u s)] (.u s)
   (fun h![a, b] => (a * b) < 2^s)
   (fun h![a, b] _ => a * b)
 
--- a - b
--- Assumption: Integer underflow throws a runtime exception.
+/--
+For `s ∈ ℕ`, defines the subtraction of two `s`-bit uints: `(a b: Tp.denote (Tp.u s))`.
+We make the following assumptions:
+- If `(a - b) ≥ 0`, then the builtin returns `(a - b) : Tp.denote (Tp.u s)`
+- Else (integer underflow), an exception is thrown.
+
+In Noir, this builtin corresponds to `a - b` for unsigned integers `a`, `b` of bit-length `s`.
+-/
 def uSub {s} := newBuiltin
   [(.u s), (.u s)] (.u s)
   (fun h![a, b] => b ≤ a)
   (fun h![a, b] _ => a - b)
 
--- a / b
--- Assumption: Divide by zero throws a runtime exception.
+/--
+For `s ∈ ℕ`, defines the division of two `s`-bit uints: `(a b: Tp.denote (Tp.u s))`.
+We make the following assumptions:
+- If `b ≠ 0`, then the builtin returns `(a / b) : Tp.denote (Tp.u s)`
+- Else (divide by zero), an exception is thrown.
+- If `a / b` is not an integer, then the result is truncated.
+
+In Noir, this builtin corresponds to `a / b` for unsigned integers `a`, `b` of bit-length `s`.
+-/
 def uDiv {s} := newBuiltin
   [(.u s), (.u s)] (.u s)
   (fun h![_, b] => b ≠ 0)
   (fun h![a, b] _ => a / b)
 
--- a % b
--- Assumption: Mod by 0 throws a runtime exception.
+/--
+For `s ∈ ℕ`, defines the modulus of two `s`-bit uints: `(a b: Tp.denote (Tp.u s))`.
+We make the following assumptions:
+- If `b ≠ 0`, then the builtin returns `(a % b) : Tp.denote (Tp.u s)`
+- Else (mod by zero), an exception is thrown.
+
+In Noir, this builtin corresponds to `a % b` for unsigned integers `a`, `b` of bit-length `s`.
+-/
 def uRem {s} := newBuiltin
   [(.u s), (.u s)] (.u s)
   (fun h![_, b] => b ≠ 0)
@@ -225,49 +265,103 @@ def bigIntDiv : Builtin := sorry
 def bigIntFromLeBytes : Builtin := sorry
 def bigIntToLeBytes : Builtin := sorry
 
--- Assumption: Out of bounds access throws a runtime exception.
+/--
+For `tp : Tp`, defines the indexing of a slice `l : Tp.denote (.slice tp)` with `i : Tp.denote (.u 32)`
+We make the following assumptions:
+- If `i < l.length`, then the builtin returns `l[i] : Tp.denote tp`
+- Else (out of bounds access), an exception is thrown.
+
+In Noir, this builtin corresponds to `T[i]` for `T: [T]` and `i: uint32`.
+-/
 def sliceIndex {tp} := newBuiltin
   [(.slice tp), (.u 32)] tp
   (fun h![l, i] => i.val < l.length)
   (fun h![l, i] v => l.get (Fin.mk i v))
 
--- Assumption: If the slice's size cannot be represented by a u32, an exception is thrown.
+/--
+For `tp : Tp`, defines the builtin that returns the length of a slice `l : Tp.denote (.slice tp)`
+We make the following assumptions:
+- If `l.length < 2^32`, then the builtin returns `l.length : Tp.denote (.u 32)`
+- Else (integer overflow), an exception is thrown.
+
+In Noir, this builtin corresponds to `fn len(self) -> u32` implemented for `[T]`.
+-/
 def sliceLen {tp} := newBuiltin
   [(.slice tp)] (.u 32)
   (fun h![l] => l.length < 2^(32))
   (fun h![l] _ => l.length)
 
--- Assumption: Slices can grow infinitely.
+/--
+For `tp : Tp`, defines the builtin that pushes back an element `e : Tp.denote tp` to a slice `l : Tp.denote (.slice tp)`.
+On these inputs, the builtin is assumed to return `l ++ [e]`.
+
+In Noir, this builtin corresponds to `fn push_back(self, elem: T) -> Self` implemented for `[T]`.
+-/
 def slicePushBack {tp} := newBuiltin
   [(.slice tp), tp] (.slice tp)
   (fun _ => True)
   (fun h![l, e] _ => l ++ [e])
 
--- Assumption: Slices can grow infinitely.
+/--
+For `tp : Tp`, defines the builtin that pushes front an element `e : Tp.denote tp` to a slice `l : Tp.denote (.slice tp)`.
+On these inputs, the builtin is assumed to return `[e] ++ l`.
+
+In Noir, this builtin corresponds to `fn push_front(self, elem: T) -> Self` implemented for `[T]`.
+-/
 def slicePushFront {tp} := newBuiltin
   [(.slice tp), tp] (.slice tp)
   (fun _ => True)
   (fun h![l, e] _ => [e] ++ l)
 
--- Assumption: Trying to insert at an index that doesn't exist throws a runtime exception.
+/--
+For `tp : Tp`, defines the insertion of an element `e : Tp.denote tp` at index `i : Tp.denote (.u 32)` to a slice `l : Tp.denote (.slice tp)`.
+We make the following assumptions:
+- If `0 ≤ i < l.length`, then the builtin returns `l'`
+where `l'` is `l` except that `e` is inserted at index `i`, and all the elements with indices larger than `i` are shifted to the right.
+- Else (out of bounds access), an exception is thrown.
+
+In Noir, this builtin corresponds to `fn insert(self, index: u32, elem: T) -> Self` implemented for `[T]`.
+-/
 def sliceInsert {tp} := newBuiltin
   [(.slice tp), (.u 32), tp] (.slice tp)
   (fun h![l, i, _] => i < l.length)
   (fun h![l, i, e] _ => List.insertNth i e l)
 
--- Assumption: Popping from an empty slice throws a runtime exception.
+/--
+For `tp : Tp`, defines the builtin that pops the first element of a slice `l : Tp.denote (.slice tp)`.
+We make the following assumptions:
+- If `l ≠ []`, then the builtin returns `(l[0], l[1:])`.
+- Else (empty slice), an exception is thrown.
+
+In Noir, this builtin corresponds to `fn pop_front(self) -> (T, Self)` implemented for `[T]`.
+-/
 def slicePopFront {tp} := newBuiltin
   [(.slice tp)] (.struct [tp, .slice tp])
   (fun h![l] => l ≠ [])
   (fun h![l] v => (l.head v, (l.tail, ())))
 
--- Assumption: Popping from an empty slice throws a runtime exception.
+/--
+For `tp : Tp`, defines the builtin that pops the last element of a slice `l : Tp.denote (.slice tp)`.
+We make the following assumptions:
+- If `l ≠ []`, then the builtin returns `(l[:l.length-1], l[l.length-1])`.
+- Else (empty slice), an exception is thrown.
+
+In Noir, this builtin corresponds to `fn pop_back(self) -> (Self, T)` implemented for `[T]`.
+-/
 def slicePopBack {tp} := newBuiltin
   [(.slice tp)] (.struct [.slice tp, tp])
   (fun h![l] => l ≠ [])
   (fun h![l] v => (l.dropLast, (l.getLast v, ())))
 
--- Assumption (5): Removing an index that doesn't exist throws a runtime exception.
+/--
+For `tp : Tp`, defines the removal of the element at the index `i : Tp.denote (.u 32)` from a slice `l : Tp.denote (.slice tp)`.
+We make the following assumptions:
+- If `i < l.length`, then the builtin returns `(l', l[i])`
+where `l'` is `l` except that the element at index `i` is removed, and all the elements with indices larger than `i` are shifted to the left.
+- Else (out of bounds access), an exception is thrown.
+
+In Noir, this builtin corresponds to `fn remove(self, index: u32) -> (Self, T)` implemented for `[T]`.
+-/
 def sliceRemove {tp} := newBuiltin
   [(.slice tp), (.u 32)] (.struct [.slice tp, tp])
   (fun h![l, i] => i.val < l.length)
