9d3df7f722da374b681067a668e9b2d831d4a8dd
utkn <utkns09@gmail.com>
2024-10-30 17:18:06 +0300

    Comptime builtins separated. Zeroed builtin implemented

--- Lampe/Lampe/Builtin/All.lean
+++ Lampe/Lampe/Builtin/All.lean
@@ -125,26 +125,4 @@ def readRef : Builtin := {
     tauto
 }
 
-def zeroed : Builtin := sorry
-def asWitness : Builtin := sorry
-
-def assertConstant : Builtin := sorry
-def staticAssert : Builtin := sorry
-
-def isUnconstrained : Builtin := sorry
-
-def derivePedersenGenerators : Builtin := sorry
-
-def qtAsTraitConstraint : Builtin := sorry
-def qtAsType : Builtin := sorry
-
-def traitConstraintEq : Builtin := sorry
-def traintConstraintHash : Builtin := sorry
-
-def traitDefAsTraitConstraint : Builtin := sorry
-
-def structDefAsType : Builtin := sorry
-def structDefGenerics : Builtin := sorry
-def structDefFields : Builtin := sorry
-
 end Lampe.Builtin
---
+++ Lampe/Lampe/Builtin/Zeroed.lean
@@ -0,0 +1,38 @@
+import Lampe.Builtin.Prelude
+
+namespace Lampe.Builtin
+open Lampe.Builtin
+
+/-- Returns a `zero` instance of a type -/
+def tpZeroed (tp : Tp) : (Tp.denote p tp) := match tp with
+| .u _ => 0
+| .i _ => 0
+| .bi => 0
+| .field => 0
+| .bool => false
+| .unit => ()
+| .str => ""
+| .slice _ => List.nil
+| .array innerTp n => Array.mk (List.replicate n.toNat (tpZeroed innerTp))
+| .ref _ => { val := 0 }
+| .struct (a :: as) => (tpZeroed a, tpZeroed (Tp.struct as))
+| .struct [] => ()
+
+/--
+For any type `tp : Tp`, this evaluates to an instance of that type such that all of its field are set to zero.
+We assume the following:
+- If `tp` is an int, uint, bigint, or a `Field`, this evaluates to `0`.
+- If `tp` is bool, this evaluates to `false`.
+- If `tp` is unit, this evaluates to `()`.
+- If `tp` is a string, this evaluates to `""`.
+- If `tp` is a slice, this evaluates to `[]`.
+- If `tp` is an array, this evaluates to an array of length `n` with all elements `zeroed`.
+- If `tp` is a reference, this evaluates to a reference with value `0`.
+- If `tp` is a struct, this evaluates to a struct with all fields `zeroed`.
+
+In Noir, this builtin corresponds to `fn zeroed<T>() -> T` implemented for `T`.
+-/
+def zeroed {tp : Tp} : Builtin := newBuiltin
+  [tp] tp (fun _ => True) (fun _ _ => tpZeroed tp)
+
+end Lampe.Builtin
