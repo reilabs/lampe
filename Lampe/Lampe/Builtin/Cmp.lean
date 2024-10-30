045bc50b7fb6f2bcdd29a69038b3953f4b8eb7de
utkn <utkns09@gmail.com>
2024-10-30 12:07:24 +0300

    Unit comparison

--- Lampe/Lampe/Builtin/Cmp.lean
+++ Lampe/Lampe/Builtin/Cmp.lean
@@ -3,17 +3,11 @@ import Lampe.Builtin.Prelude
 namespace Lampe.Builtin
 open Lampe.Builtin
 
-/-- a == b -/
-def refEq {tp : Tp}: Builtin := newBuiltin
-  [(.ref tp), (.ref tp)] .bool
-  (fun _ => True)
-  sorry
-
-/-- a == b -/
+/-- () = () -/
 def unitEq : Builtin := newBuiltin
   [(.unit), (.unit)] .bool
   (fun _ => True)
-  sorry
+  (fun _ _ => True)
 
 /-- a == b -/
 def bEq : Builtin := newBuiltin
@@ -22,39 +16,80 @@ def bEq : Builtin := newBuiltin
   (fun h![a, b] _ => a == b)
 
 /-- a == b -/
-def uEq {s : Nat}: Builtin := newBuiltin
+def uEq {s : Nat} : Builtin := newBuiltin
   [(.u s), (.u s)] .bool
   (fun _ => True)
   (fun h![a, b] _ => a == b)
 
 /-- a == b -/
-def iEq {s : Nat}: Builtin := newBuiltin
+def iEq {s : Nat} : Builtin := newBuiltin
   [(.i s), (.i s)] .bool
   (fun _ => True)
   (fun h![a, b] _ => a == b)
 
+/-- a == b -/
+def fEq : Builtin := newBuiltin
+  [(.field), (.field)] .bool
+  (fun _ => True)
+  (fun h![a, b] _ => a == b)
+
 /-- a < b -/
-def uLt {s : Nat}: Builtin := newBuiltin
+def uLt {s : Nat} : Builtin := newBuiltin
   [(.u s), (.u s)] .bool
   (fun _ => True)
   (fun h![a, b] _ => a < b)
 
 /-- a < b -/
-def iLt {s : Nat}: Builtin := newBuiltin
+def iLt {s : Nat} : Builtin := newBuiltin
   [(.i s), (.i s)] .bool
   (fun _ => True)
   (fun h![a, b] _ => a < b)
 
 /-- a > b -/
-def uGt {s : Nat}: Builtin := newBuiltin
+def uGt {s : Nat} : Builtin := newBuiltin
   [(.u s), (.u s)] .bool
   (fun _ => True)
   (fun h![a, b] _ => a > b)
 
 /-- a > b -/
-def iGt {s : Nat}: Builtin := newBuiltin
+def iGt {s : Nat} : Builtin := newBuiltin
   [(.i s), (.i s)] .bool
   (fun _ => True)
   (fun h![a, b] _ => a > b)
 
-end Lampe.Builtin
+-- @[reducible]
+-- def Ord := Tp.struct [Tp.field]
+-- def Ord.Less : Tp.denote p Ord := (0, ())
+-- def Ord.Equal : Tp.denote p Ord := (1, ())
+-- def Ord.Greater : Tp.denote p Ord := (2, ())
+
+-- class NoirCmp (α : Type) where
+--   cmp : {p : Prime} -> α -> α -> (Tp.denote p Ord)
+
+-- instance {s} : NoirCmp (Tp.denote p (.i s)) where
+--   cmp := fun a b =>
+--     if a < b then
+--       Ord.Less
+--     else if a > b then
+--       Ord.Greater
+--     else
+--       Ord.Equal
+
+-- instance {s} : NoirCmp (Tp.denote p (.u s)) where
+--   cmp := fun a b =>
+--     if a < b then
+--       Ord.Less
+--     else if a > b then
+--       Ord.Greater
+--     else
+--       Ord.Equal
+
+-- instance : NoirCmp (Tp.denote p .unit) where
+--   cmp := fun _ _ => Ord.Equal
+
+-- instance : NoirCmp (Tp.denote p .bool) where
+--   cmp := fun a b => match a, b with
+--   | true, true => Ord.Equal
+--   | true, false => Ord.Greater
+--   | false, true => Ord.Less
+--   | false, false => Ord.Equal
