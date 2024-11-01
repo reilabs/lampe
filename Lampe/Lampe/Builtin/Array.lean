86c3c57d53eef2825a3ce6e78eebed6239061459
utkn <utkns09@gmail.com>
2024-11-01 21:03:06 +0300

    String and array builtins converted to manual proofs

--- Lampe/Lampe/Builtin/Array.lean
+++ Lampe/Lampe/Builtin/Array.lean
@@ -2,25 +2,53 @@ import Lampe.Builtin.Basic
 namespace Lampe.Builtin
 open Lampe.Builtin
 
+inductive arrayLenOmni : Omni where
+| mk {P st Q n a} :
+  Q (some (st, n)) → arrayLenOmni P st [.array tp n] (.u 32) h![a] Q
+
 /--
 Defines the function that evaluates to an array's length `n`.
 This builtin evaluates to an `U 32`. Hence, we assume that `n < 2^32`.
 
 In Noir, this corresponds to `fn len(self) -> u32` implemented for `[_; n]`.
 -/
-def arrayLen {tp : Tp} {n : U 32} : Builtin := newPureBuiltin
-  [(.array tp n)] (.u 32)
-  (fun _ => True)
-  (fun _ _ => n)
+def arrayLen : Builtin := {
+  omni := arrayLenOmni
+  conseq := by
+    unfold omni_conseq
+    intros
+    cases_type arrayLenOmni
+    tauto
+  frame := by
+    unfold omni_frame
+    intros
+    cases_type arrayLenOmni
+    constructor
+    constructor <;> tauto
+}
+
+inductive arrayAsSliceOmni : Omni where
+| mk {P st Q n a} :
+  Q (some (st, a.toList)) → arrayAsSliceOmni P st [.array tp n] (.slice tp) h![a] Q
 
 /--
 Defines the function that converts an array to a slice.
 
 In Noir, this corresponds to `fn as_slice(self) -> [T]` implemented for `[T; n]`.
 -/
-def arrayAsSlice {tp : Tp} {n : U 32}: Builtin := newPureBuiltin
-  [(.array tp n)] (.slice tp)
-  (fun _ => True)
-  (fun h![a] _ => a.toList)
+def arrayAsSlice : Builtin := {
+  omni := arrayAsSliceOmni
+  conseq := by
+    unfold omni_conseq
+    intros
+    cases_type arrayAsSliceOmni
+    tauto
+  frame := by
+    unfold omni_frame
+    intros
+    cases_type arrayAsSliceOmni
+    constructor
+    constructor <;> tauto
+}
 
 end Lampe.Builtin
--- Lampe/Lampe/Builtin/Str.lean
+++ Lampe/Lampe/Builtin/Str.lean
@@ -2,15 +2,37 @@ import Lampe.Builtin.Basic
 namespace Lampe.Builtin
 open Lampe.Builtin
 
+inductive strAsBytesOmni : Omni where
+| ok {P st n s Q} :
+  (h: s.toList.length = n.toNat)
+    → Q (some (st, .map (fun u => u.toNat) ⟨s.toList, (h)⟩))
+    → strAsBytesOmni P st [.str n] (.array (.u 8) n) h![s] Q
+| err {P st n s Q} :
+  (s.toList.length ≠ n.toNat)
+    → Q none
+    → strAsBytesOmni P st [.str n] (.array (.u 8) n) h![s] Q
+
 /--
 Defines the conversion of strings of length `n` to a byte array of length `n`.
 Accordingly, we assume that the string has UTF-8 byte length of `n`.
 
 In Noir, this corresponds to `fn as_bytes(self) -> [u8; n]` implemented for `str<n>`.
 -/
-def strAsBytes {n : U 32} : Builtin := newPureBuiltin
-  [.str n] (.array (.u 8) n)
-  (fun h![s] => s.toList.length = n.toNat)
-  (fun h![s] h => .map (fun u => u.toNat) ⟨s.toList, (h)⟩)
+def strAsBytes : Builtin := {
+  omni := strAsBytesOmni
+  conseq := by
+    unfold omni_conseq
+    intros
+    cases_type strAsBytesOmni
+    . constructor <;> simp_all
+    . apply strAsBytesOmni.err <;> simp_all
+  frame := by
+    unfold omni_frame
+    intros
+    cases_type strAsBytesOmni
+    . constructor
+      . constructor <;> tauto
+    . apply strAsBytesOmni.err <;> assumption
+}
 
 end Lampe.Builtin
