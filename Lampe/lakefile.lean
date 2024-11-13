b0ffff5139bc4a49821b70d78d4eab8873ea7a77
Utkan <utkns09@gmail.com>
2024-11-13 13:32:14 +0300

    Update to Lean v4.12.0 (#15)
    Updated the toolchain to v4.12.0

--- Lampe/lakefile.lean
+++ Lampe/lakefile.lean
@@ -8,8 +8,7 @@ package «Lampe» where
   ]
   -- add any additional package configuration options here
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require "leanprover-community" / "mathlib" @ git "v4.12.0"
 
 @[default_target]
 lean_lib «Lampe» where
