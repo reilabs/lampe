3928b654c4bb54d71cfdcfba5dcbc2f9546a23cf
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2025-01-17 15:17:26 -0500

    Add Lean testing framework (#38)
    * (wip) Move examples Lampe.lean -> Tests/Tests.lean
    
    * Update lakefile
    
    * Add `Tests` lib and test driver
    * Remove quotes from package and library names
    * Clean up auto-generated comments
    
    * Update CI

--- Lampe/Lampe/Basic.lean
+++
@@ -1,9 +0,0 @@
-import Lampe.Semantics
-import Lampe.Syntax
-import Lampe.Ast
-import Lampe.Tp
-import Lampe.Lens
-import Lampe.Hoare.Total
-import Lampe.Hoare.SepTotal
-import Lampe.Tactic.SeparationLogic
-import Lampe.Tactic.Traits
