d998a283c7cd5d47e2b32955d79638b5e71c3839
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2025-03-10 12:29:48 -0400

    minimal example (#69)

---
+++ Examples/example1/Example1.lean
@@ -0,0 +1,3 @@
+import Example1.Extracted
+import Example1.Ref
+import Example1.Spec
---
+++ Examples/example1/Example1/Ref.lean
@@ -0,0 +1,9 @@
+import Lampe
+
+namespace Ref
+
+open Lampe
+
+variable {p : Prime}
+
+def main (x y : Fp p) : Fp p := x + y
