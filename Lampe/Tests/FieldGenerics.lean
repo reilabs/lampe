b01e36cd9ba8c1049a1c5a96d4d66d29c6cd243f
Matej Penciak <matej.penciak@gmail.com>
2025-03-20 14:59:27 -0400

    example proved

--- Lampe/Tests/FieldGenerics.lean
+++ Lampe/Tests/FieldGenerics.lean
@@ -6,6 +6,7 @@ nr_def «A»<>() -> Field {
     4294967297 : Field
 }
 
+
 nr_def «foo»<@A : type_fp>() -> Field {
     f@A
 }
@@ -14,4 +15,32 @@ nr_def «test»<>() -> Field {
     let a = (@foo<4294967297 : type_fp> as λ() → Field)();
     a
 }
+
+def env := Lampe.Env.mk [«A», «foo», «test»] []
+
+lemma A_intro : STHoare p env ⟦⟧ (A.call h![] h![]) fun output => output = (4294967297 : Fp p) := by
+    enter_decl
+    steps
+    subst_vars
+    rfl
+
+lemma foo_intro : STHoare p env ⟦⟧ (foo.call h![gen] h![]) fun output => output = (gen : Fp p) := by
+    enter_decl
+    steps [A_intro]
+    subst_vars
+    rfl
+
+lemma test_intro : STHoare p env ⟦⟧ (test.call h![] h![]) fun output => output = (4294967297 : Fp p) := by
+    enter_decl
+    steps [A_intro, foo_intro]
+
+    apply STHoare.letIn_intro
+    enter_decl
+    steps
+
+    apply STHoare.var_intro
+    intros
+    steps
+    subst_vars
+    rfl
 
