255f6619496d3ee84ce4a47cf3ec2a90b6187a85
Piotr Heilman <piotr.heilman@gmail.com>
2025-03-27 14:42:55 +0100

    Fix e2e tests.

--- Examples/MerkleFromScratch/user_actions.sh
+++ Examples/MerkleFromScratch/user_actions.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 set -euxo pipefail
 
-cp ./../Merkle/lampe/Field.lean ./lampe
+cp ./user_files/Field.lean ./lampe
 echo "import Field" >> ./lampe/Merkle.lean
 echo "" >> ./lampe/lakefile.toml
 echo "[[lean_lib]]" >> ./lampe/lakefile.toml
 echo "name = \"Field\"" >> ./lampe/lakefile.toml
 
-cp ./../Merkle/lampe/Ref.lean ./lampe
+cp ./user_files/Ref.lean ./lampe
 echo "import Ref" >> ./lampe/Merkle.lean
 echo "" >> ./lampe/lakefile.toml
 echo "[[lean_lib]]" >> ./lampe/lakefile.toml
 echo "name = \"Ref\"" >> ./lampe/lakefile.toml
 
-cp ./../Merkle/lampe/Spec.lean ./lampe
+cp ./user_files/Spec.lean ./lampe
 echo "import Spec" >> ./lampe/Merkle.lean
 echo "" >> ./lampe/lakefile.toml
 echo "[[lean_lib]]" >> ./lampe/lakefile.toml
