6f3ffbc620c31a61bb026255fdadb53534002560
Matej Penciak <matej.penciak@gmail.com>
2025-04-25 17:42:06 -0400

    (wip): Somewhat working unexpander

--- Lampe/Lampe/Delab.lean
+++ Lampe/Lampe/Delab.lean
@@ -21,7 +21,7 @@ syntax "field" : pp_tp
 syntax "[" pp_tp "]" : pp_tp
 syntax "[" pp_tp ";" num "]" : pp_tp
 syntax "(" pp_tp,* ")" : pp_tp
-syntax "ref" pp_tp : pp_tp
+syntax "refTp" pp_tp : pp_tp
 syntax "λ(" pp_tp,* ")" "->" pp_tp : pp_tp
 
 def delabTpU : TSyntax `term → (UnexpandM <| TSyntax `pp_tp)
@@ -96,7 +96,7 @@ partial def delabTpTuple : TSyntax `term → (UnexpandM <| TSyntax `pp_tp)
 | _ => throw ()
 
 partial def delabTpRef : TSyntax `term → (UnexpandM <| TSyntax `pp_tp)
-| `($(_) $tp) => do `(pp_tp|ref $(← delabTp tp))
+| `($(_) $tp) => do `(pp_tp|refTp $(← delabTp tp))
 | _ => throw ()
 
 -- TODO: Finish this
@@ -144,29 +144,40 @@ def delabKindType : TSyntax `term → (UnexpandM <| TSyntax `pp_kind)
 | `($(_)) => `(pp_kind|type)
 
 declare_syntax_cat pp_expr
-syntax ">>>" pp_expr "<<<" : term
+declare_syntax_cat pp_fn_binders
+declare_syntax_cat pp_fn_generics
+declare_syntax_cat pp_kinds
 
+syntax "⟪" ppLine pp_expr ppLine "⟫" : term
+syntax "@(" term ")": pp_expr
 syntax num : pp_expr -- litNums, constFps, and constUs
+syntax str : pp_expr -- litStrs, and fmtStrs
+syntax ident : pp_kinds
+syntax "<" pp_kinds,* ">" : pp_fn_generics
+syntax "(" pp_expr,* ")" : pp_fn_binders
+syntax "λ#" num pp_fn_binders : pp_expr -- lambda funcref
+syntax ident pp_fn_generics pp_fn_binders : pp_expr -- decl and trait funcrefs
+syntax ident : pp_expr -- Get
+syntax ppDedent("let " ident " := " pp_expr ppHardLineUnlessUngrouped pp_expr) : pp_expr
+syntax "#" ident pp_fn_binders : pp_expr -- builtin call
+syntax "if" pp_expr "then" pp_expr "else" pp_expr : pp_expr
+syntax "skip" : pp_expr
+syntax "for" pp_expr "in" pp_expr "do" pp_expr : pp_expr
+
 instance : Coe NumLit (TSyntax `pp_expr) where
   coe s := ⟨s.raw⟩
 
-syntax str : pp_expr -- litStrs, and fmtStrs
 instance : Coe StrLit (TSyntax `pp_expr) where
   coe s := ⟨s.raw⟩
 
--- declare_syntax_cat letin
--- declare_syntax_cat
-
--- syntax term : expr
--- syntax letin : expr
--- syntax ident noWs "<" sepBy(expr, ",", ",", allowTrailingSep) ">" noWs "(" sepBy(expr, ",", ",", allowTrailingSep) ")" : expr
--- syntax "let" ident ws "=" ws expr ";" : letin
+instance : Coe Lean.Ident (TSyntax `pp_expr) where
+  coe s := ⟨s.raw⟩
 
 @[app_unexpander Expr.litNum]
 def delabLitNum : Unexpander
-| `($(_) $_ $n:num) => `(>>> $n <<<)
-| _ => pure
-
+| `($(_) $_ $n:num) => `(⟪ $n ⟫)
+| `($(_) $_ $num) => `(⟪@($num)⟫)
+| stx => pure stx
 
 @[app_unexpander Expr.litStr]
 def delabLitStr : Unexpander
@@ -177,49 +188,135 @@ def delabLitStr : Unexpander
     if let some c := c?.isCharLit? then
       string := string.push c
   let stringStx := Syntax.mkStrLit string
-  `(>>> $stringStx <<<)
-| _ => pure
-
+  `(⟪ $stringStx ⟫)
+| stx => pure stx
 
 @[app_unexpander Expr.constFp]
 def delabConstFp : Unexpander
-| `($(_) $n:num $_) => `(>>> $n <<<)
-| _ => pure
+| `($(_) $n:num $_) => `(⟪ $n ⟫)
+| `($(_) $n $_) => `(⟪@($n)⟫)
+| stx => pure stx
 
 @[app_unexpander Expr.constU]
 def delabConstU : Unexpander
-| `($(_) $n:num $_) => `(>>> $n <<<)
-| _ => pure
-
+| `($(_) $n:num $_) => `(⟪ $n ⟫)
+| `($(_) $n $_) => `(⟪@($n)⟫)
+| stx => pure stx
 
 @[app_unexpander Expr.fmtStr]
 def delabFmtStr : Unexpander
-| `($(_) $_ $_ $_) => sorry
-| _ => pure
+| `($(_) $_ $_ $fmtstr) => `(⟪@($fmtstr)⟫) -- TODO: finish this
+| stx => pure stx
 
-declare_syntax_cat pp_fn_binders
-declare_syntax_cat pp_fn_generics
-declare_syntax_cat pp_kinds
-
-syntax ident : pp_kinds
-syntax "<"pp_kinds,*">" : pp_fn_generics
-syntax "(" pp_expr,* ")" : pp_fn_binders
-
-syntax "λ#" num pp_fn_binders : pp_expr -- lambda funcref
-syntax ident pp_fn_generics pp_fn_binders : pp_expr -- decl and trait funcrefs
+syntax ">>>" term "<<<" : pp_expr
 
 @[app_unexpander Expr.fn]
 def delabFn : Unexpander
-| `($(_) $argTps $outTp $funcref) =>
+| `($(_) $_ $_ $funcref) =>
   match funcref with
-  | `(``FuncRef.lambda $ref) => `(>>> λ#3 () <<<)
-  | `(``FuncRef.decl $name $kinds $generics) => `(>>>  <<<)
-  | `(``FuncRef.trait $selfTp $traitName $kinds $generics $fnName $fnKinds $fnGenerics) => sorry
-  | _ => pure
+  | `(FuncRef.lambda $ref) => do
+    match ref with
+    | `($(_) $n:num) => do
+      if let some n := n.raw.isNatLit? then
+        let n := Syntax.mkNatLit n
+        `(⟪ λ#$n() ⟫)
+      else
+        throw ()
+    | stx => pure stx
+  | `(FuncRef.decl $name:str [$kinds,*] h![$generics,*]) => do
+    let name := mkIdent <| .mkSimple <| name.raw.isStrLit?.getD ""
+    `(⟪ $name:ident <> ()  ⟫)
+  | `(FuncRef.trait $selfTp $traitName $kinds $generics $fnName $fnKinds $fnGenerics) => do
+    let name := mkIdent traitName.raw.getId
+    let fnName := mkIdent fnName.raw.getId
+    `(⟪ $name:ident <> ()  ⟫)
+  | stx => pure stx
+| stx => pure stx
+
+@[app_unexpander Expr.var]
+def delabVar : Unexpander
+| `($(_) $var:ident) =>
+  `(⟪ $var ⟫)
+| stx => pure stx
+
+@[app_unexpander Expr.letIn]
+def delabLetIn : Unexpander
+| `($(_) ⟪$a⟫ fun $v => ⟪$body⟫) =>
+  let binding := mkIdent v.raw.getId
+  `(⟪ let $binding := $a $body ⟫)
+| `($(_) $a fun $v => ⟪$body⟫) =>
+  let binding := mkIdent v.raw.getId
+  `(⟪ let $binding := @($a) $body⟫)
+| stx => pure stx
+
+
+@[app_unexpander Expr.call]
+def delabCall : Unexpander
+| `($(_) $_ $_ $funcRef $args) => do
+  `(⟪@($funcRef:term)⟫)
+| stx => pure stx
+
+-- TODO: Will likely need this
+-- @[app_unexpander Lampe.FunctionDecl.call]
+-- def delabFunctionDeclCall : Unexpander
+-- | `($(_) $decl $gens $args) => do
+--   let x := mkIdent decl.raw.getId
+--   `($x <$gens> ($args))
+-- | _ => pure
+
+@[app_unexpander Expr.callBuiltin]
+def delabCallBuiltin : Unexpander
+| `($(_) $_ $_ $builtin h![$args,*]) =>
+  let builtin := mkIdent builtin.raw.getId
+  `(⟪ #$builtin:ident () ⟫)
+| stx => pure stx
+
+
+@[app_unexpander Expr.ite]
+def delabITE : Unexpander
+| `($(_) $cond ⟪$ifTrue⟫ ⟪$ifFalse⟫) =>
+  let cond := mkIdent cond.raw.getId
+  `(⟪ if $cond then $ifTrue else $ifFalse ⟫)
+| stx => pure stx
+
+@[app_unexpander Expr.skip]
+def delabSkip : Unexpander
+| `($(_)) =>
+  `(⟪ skip ⟫)
+
+
+@[app_unexpander Expr.loop]
+def delabLoop : Unexpander
+| `($(_) $low $high (fun $var => ⟪$body⟫)) =>
+  let low := mkIdent low.raw.getId
+  let high := mkIdent high.raw.getId
+  let _ := mkIdent var.raw.getId
+  `(⟪ for $low in $high do $body ⟫)
+| stx => pure stx
+
+-- TODO: gotta do this
+@[app_unexpander Lampe.Expr.lam]
+def delabLam : Unexpander
+| stx => pure stx
+
+section STHoare
+
+declare_syntax_cat slp_cond
+declare_syntax_cat sthoare
+
+syntax "⦃" term "⦄" : slp_cond
+syntax slp_cond ppLine ppLine term ppLine ppLine slp_cond : term
+
+@[app_unexpander STHoare]
+def delabSTHoare : Unexpander
+| `($(_) $_ $_ $P ⟪$C⟫ $Q) =>
+  `(⦃$P⦄ ⟪$C⟫ ⦃$Q⦄)
+| `($(_) $_ $_ $P $C $Q) =>
+  `(⦃$P⦄ $C ⦃$Q⦄)
 | _ => pure
 
 nr_def simple_muts<>(x : Field) -> Field {
-  #fAdd(x, 1 : Field) : Field
+  #fAdd(x, 1 : Field) : Field;
 }
 
 nr_def simple_muts2<>(x : Field) -> Field {
@@ -235,132 +332,6 @@ example : STHoare p Γ ⟦⟧ (simple_muts2.call h![] h![x]) fun v => v = x := b
   steps
   simp_all
 
-@[app_unexpander Expr.var]
-def delabVar : Unexpander
-| `($(_) $var) => sorry
-| _ => pure
-
-@[app_unexpander Expr.letIn]
-def delabLetIn : Unexpander
-| `($(_) $a fun $v => $body) => sorry
-  -- let bIdent := mkIdent `XYZW
-  -- let binding := mkIdent v.raw.getId
-  -- `(letin| let $binding = $a;)
-| _ => pure
-
-@[app_unexpander Expr.call]
-def delabCall : Unexpander
-| `($(_) $_ $_) => sorry -- TODO: This one will be tricky I think
-| _ => pure
-
--- TODO: Will likely need this
--- @[app_unexpander Lampe.FunctionDecl.call]
--- def delabFunctionDeclCall : Unexpander
--- | `($(_) $decl $gens $args) => do
---   let x := mkIdent decl.raw.getId
---   `($x <$gens> ($args))
--- | _ => pure
-
-@[app_unexpander Expr.callBuiltin]
-def delabCallBuiltin : Unexpander
-| `($(_) $_ $_ $builtin $args) => sorry
-| _ => pure
-
-@[app_unexpander Expr.ite]
-def delabITE : Unexpander
-| `($(_) $cond $ifTrue $ifFalse) => sorry
-| _ => pure
-
-@[app_unexpander Expr.skip]
-def delabSkip : Unexpander
-| `($(_)) => sorry
-
-@[app_unexpander Expr.loop]
-def delabLoop : Unexpander
-| `($(_) $low $high (fun $var => $body)) => sorry
-| _ => pure
-
-@[app_unexpander Lampe.Expr.lam]
-def delabLam : Unexpander
-| `($(_) $_ $_ fun $args => $body) => sorry
-| _ => pure
-
-section STHoare
-
-declare_syntax_cat slp_cond
-declare_syntax_cat sthoare
-
-syntax "⦃" term "⦄" : slp_cond
-syntax slp_cond ppLine ppLine ppGroup(pp_expr) ppLine ppLine slp_cond : term
-
-@[app_unexpander STHoare]
-def delabSTHoare : Unexpander
-| `($(_) $_ $_ $P $C $Q) =>
-  `(⦃$P⦄ $C ⦃$Q⦄)
-| _ => pure
 
 end STHoare
 
-
-declare_syntax_cat lang
-syntax num   : lang
-syntax ident : lang
-syntax "let " ident " := " lang " in " lang: lang
-syntax "[Lang| " lang "]" : term
-
-inductive LangExpr
-  | numConst : Nat → LangExpr
-  | ident    : String → LangExpr
-  | letE     : String → LangExpr → LangExpr → LangExpr
-
-macro_rules
-  | `([Lang| $x:num ]) => `(LangExpr.numConst $x)
-  | `([Lang| $x:ident]) => `(LangExpr.ident $(Lean.quote (toString x.getId)))
-  | `([Lang| let $x:ident := $v:lang in $b:lang]) => `(LangExpr.letE $(Lean.quote (toString x.getId)) [Lang| $v] [Lang| $b])
-
-instance : Coe NumLit (TSyntax `lang) where
-  coe s := ⟨s.raw⟩
-
-instance : Coe Lean.Ident (TSyntax `lang) where
-  coe s := ⟨s.raw⟩
-
--- LangExpr.letE "foo" (LangExpr.numConst 12)
---   (LangExpr.letE "bar" (LangExpr.ident "foo") (LangExpr.ident "foo")) : LangExpr
-#check [Lang|
-  let foo := 12 in
-  let bar := foo in
-  foo
-]
-
-@[app_unexpander LangExpr.numConst]
-def unexpandNumConst : Unexpander
-  | `($_numConst $x:num) => `([Lang| $x])
-  | _ => throw ()
-
-@[app_unexpander LangExpr.ident]
-def unexpandIdent : Unexpander
-  | `($_ident $x:str) =>
-    let str := x.getString
-    let name := mkIdent $ Name.mkSimple str
-    `([Lang| $name])
-  | _ => throw ()
-
-@[app_unexpander LangExpr.letE]
-def unexpandLet : Unexpander
-  | `($_letE $x:str [Lang| $v:lang] [Lang| $b:lang]) =>
-    let str := x.getString
-    let name := mkIdent $ Name.mkSimple str
-    `([Lang| let $name := $v in $b])
-  | _ => throw ()
-
--- [Lang| let foo := 12 in foo] : LangExpr
-#check [Lang|
-  let foo := 12 in foo
-]
-
--- [Lang| let foo := 12 in let bar := foo in foo] : LangExpr
-#check [Lang|
-  let foo := 12 in
-  let bar := foo in
-  foo
-]
