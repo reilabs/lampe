a127f5fcf1a72e1c5094093bdfeb5c206f1f2b41
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2025-05-27 07:29:17 -0400

    fix: namespaces for impls and structs (#94)
    * add a regression test for the fix
    
    * fix: namespaces for impls and structs
    
    * fix: array and slice namespaces are treated separately

--- Examples/ExtractionTests/lampe/ExtractionTests-0.0.0/Extracted/ExtractionTests-0.0.0.lean
+++ Examples/ExtractionTests/lampe/ExtractionTests-0.0.0/Extracted/ExtractionTests-0.0.0.lean
@@ -4,6 +4,7 @@ import «ExtractionTests-0.0.0».Extracted.ConstGenerics
 import «ExtractionTests-0.0.0».Extracted.GeneratedTypes
 import «ExtractionTests-0.0.0».Extracted.Lib
 import «ExtractionTests-0.0.0».Extracted.MultipleGenerics
+import «ExtractionTests-0.0.0».Extracted.StructNamespaces
 
 namespace «ExtractionTests-0.0.0»
 namespace Extracted
@@ -11,3 +12,4 @@ namespace Extracted
 def env := «ExtractionTests-0.0.0».Extracted.ConstGenerics.env
   ++ «ExtractionTests-0.0.0».Extracted.Lib.env
   ++ «ExtractionTests-0.0.0».Extracted.MultipleGenerics.env
+  ++ «ExtractionTests-0.0.0».Extracted.StructNamespaces.env
