61eff85a5e33ab26683fd63ec734437d20da47f1
Piotr Heilman <piotr.heilman@gmail.com>
2025-05-29 10:00:22 +0200

    Allow to select single test. Support list of expected results.

--- noir_testing/test.sh
+++ noir_testing/test.sh
@@ -4,6 +4,7 @@ set -e
 usage(){
 >&2 cat << EOF
 Usage: $0
+   [ -t | --test  ] Name of directory (full path, ex. '/home/user/noir/noir_test_success/should_fail_with_matches') with test to run
    [ --noir-path  ] Path to noir repository
    [ --lampe-cmd  ] Lampe command (default: lampe)
    [ --lake-cmd   ] Lake command (default: lake)
@@ -19,6 +20,7 @@ do
 		break
 	fi
   case $1 in
+    -t | --test)          PARAM_TEST=$2          ; shift 2 ;;
     --noir-path)          PARAM_NOIR_PATH=$2     ; shift 2 ;;
     --lampe-cmd)          PARAM_LAMPE_CMD=$2     ; shift 2 ;;
     --lake-cmd)           PARAM_LAKE_CMD=$2      ; shift 2 ;;
@@ -42,27 +44,36 @@ if [[ ! -d ${TEST_PROGRAMS_PATH} ]]; then
 	exit 1
 fi
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SELECTED_TEST="${PARAM_TEST:-}"
 LAMPE_CMD="${PARAM_LAMPE_CMD:-lampe}"
 LAKE_CMD="${PARAM_LAKE_CMD:-lake}"
-LOG_DIR="${PARAM_LOG_DIR:-$(pwd)}"
+LOG_DIR="${PARAM_LOG_DIR:-${SCRIPT_DIR}}"
 LOG_STDOUT="${PARAM_LOG_STDOUT:-false}"
 LOG_FILE="${LOG_DIR}/running.log"
-LAKE_DIR="$(pwd)/.lake"
+LAKE_DIR="${SCRIPT_DIR}/.lake"
 
 if [ ! -d ${LAKE_DIR} ]; then
 	mkdir -p "${LAKE_DIR}"
 fi
 
-readarray -t test_dirs_1 < <(find "$TEST_PROGRAMS_PATH/compile_success_contract" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-readarray -t test_dirs_2 < <(find "$TEST_PROGRAMS_PATH/compile_success_empty" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-readarray -t test_dirs_3 < <(find "$TEST_PROGRAMS_PATH/compile_success_no_bug" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-readarray -t test_dirs_4 < <(find "$TEST_PROGRAMS_PATH/compile_success_with_bug" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-readarray -t test_dirs_5 < <(find "$TEST_PROGRAMS_PATH/execution_success" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-readarray -t test_dirs_6 < <(find "$TEST_PROGRAMS_PATH/noir_test_success" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
-readarray -t test_dirs_7 < <(find "$TEST_PROGRAMS_PATH/test_libraries" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+if [[ "$SELECTED_TEST" == "" ]]; then
+	readarray -t test_dirs_1 < <(find "$TEST_PROGRAMS_PATH/compile_success_contract" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+	readarray -t test_dirs_2 < <(find "$TEST_PROGRAMS_PATH/compile_success_empty" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+	readarray -t test_dirs_3 < <(find "$TEST_PROGRAMS_PATH/compile_success_no_bug" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+	readarray -t test_dirs_4 < <(find "$TEST_PROGRAMS_PATH/compile_success_with_bug" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+	readarray -t test_dirs_5 < <(find "$TEST_PROGRAMS_PATH/execution_success" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+	readarray -t test_dirs_6 < <(find "$TEST_PROGRAMS_PATH/noir_test_success" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
+	readarray -t test_dirs_7 < <(find "$TEST_PROGRAMS_PATH/test_libraries" -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*')
 
-test_dirs=( "${test_dirs_1[@]}" "${test_dirs_2[@]}" "${test_dirs_3[@]}" "${test_dirs_4[@]}" "${test_dirs_5[@]}" "${test_dirs_6[@]}" "${test_dirs_7[@]}" )
+	test_dirs=( "${test_dirs_1[@]}" "${test_dirs_2[@]}" "${test_dirs_3[@]}" "${test_dirs_4[@]}" "${test_dirs_5[@]}" "${test_dirs_6[@]}" "${test_dirs_7[@]}" )
+else
+	test_dirs=( "$SELECTED_TEST" )
+fi
 
+# This call logs info with prefix '[0xa1c]'. This is just a random value
+# used to make it easier to grep for its output instead of other (lake,
+# lampe, etc) programs being run inside.
 do_process_test() {
     local dir=$1
     local lake_dir=$2
@@ -81,7 +92,9 @@ do_process_test() {
 
 		cd lampe
 
-		ln -s ${lake_dir} .lake
+		if [ ! -d .lake ]; then
+			ln -s ${lake_dir} .lake
+		fi
 		if ! ${lake_cmd} exe cache get; then
 			echo "[0xa1c] failed $dir"
 			return 0
@@ -112,19 +125,51 @@ for (( i=0; i<$num_test_cases; i++ )); do
 done
 
 if [ -f "$LOG_FILE" ]; then
+		# '[0xa1c]' is a prefix used by output of running test by this script
     failed_dirs=($(grep -a '\[0xa1c\] failed' "$LOG_FILE" | awk '{print $3}'))
+    succeeded_dirs=($(grep -a '\[0xa1c\] succeeded' "$LOG_FILE" | awk '{print $3}'))
 else
     echo "$LOG_FILE not found or empty. Check for errors." >&2
     exit 1
 fi
 
-if [ ${#failed_dirs[@]} -ne 0 ]; then
-    echo "Test failures (${#failed_dirs[@]}/${num_test_cases}) for the following directories:"
-    for dir in "${failed_dirs[@]}"; do
-        echo "- $dir"
+ESCAPED_TEST_PROGRAMS_PATH="$(echo ${TEST_PROGRAMS_PATH} | sed -e 's/\//\\\//g')"
+
+unexpected_fails=()
+for test_dir in "${failed_dirs[@]}"; do
+	found=$(cat ${SCRIPT_DIR}/should_fail | sed -e "s/^/${ESCAPED_TEST_PROGRAMS_PATH}\//" | grep $test_dir | wc -l)
+	if [ ${found} -eq 0 ]; then
+		echo "Test ${test_dir} failed but was not expected to do so"
+		unexpected_fails+=("${test_dir}")
+	else
+		echo "Test ${test_dir} - failed as expected"
+	fi
+done
+
+unexpected_succeeds=()
+for test_dir in "${succeeded_dirs[@]}"; do
+	found=$(cat ${SCRIPT_DIR}/should_succeed | sed -e "s/^/${ESCAPED_TEST_PROGRAMS_PATH}\//" | grep $test_dir | wc -l)
+	if [ ${found} -eq 0 ]; then
+		echo "Test ${test_dir} - succeeded but was not expected to do so"
+		unexpected_succeeds+=("${test_dir}")
+	else
+		echo "Test ${test_dir} - succeeded as expected"
+	fi
+done
+
+count_expected_mismatch=$(( ${#unexpected_fails[@]} + ${#unexpected_succeeds[@]} ))
+if [ ${count_expected_mismatch} -ne 0 ]; then
+    echo "Unexpected results (${count_expected_mismatch}/${num_test_cases}) for the run. Fail."
+    echo "Unexpected fails:"
+    for test_dir in "${unexpected_fails[@]}"; do
+    	echo "- ${test_dir}"
+    done
+    echo "Unexpected succeeds:"
+    for test_dir in "${unexpected_succeeds[@]}"; do
+    	echo "- ${test_dir}"
     done
     echo "Check ${LOG_FILE} for details."
     exit 1
 else
-    echo "Test succeeded!"
+    echo "All run as expected! Success!"
 fi
