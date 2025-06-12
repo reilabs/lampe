905b7f5c4c604fd0ac9df629258e889ace28f1be
Matej Penciak <matej.penciak@gmail.com>
2025-06-12 11:06:05 -0400

    docs, cleanup, and tracing

--- Lampe/Lampe/Tactic/Traits.lean
+++ Lampe/Lampe/Tactic/Traits.lean
@@ -5,78 +5,41 @@ import Lampe.Syntax
 
 open Lean Elab.Tactic Parser.Tactic Lean.Meta Qq
 
-partial def extractImpl (expr : Lean.Expr) : TacticM $ TSyntax `term := do
-  match_expr expr with
-  | Prod.mk _ _ _name impl => return ← impl.toSyntax
-  | _ => throwError "Didn't expect that..."
+initialize
+  registerTraceClass `Lampe.Traits
 
-partial def extractAllImpls (envExpr : Expr) : TacticM $ List (TSyntax `term) := do
+/-- Extract all the `TraitImpl`s from a `Lampe.Env` -/
+partial def extractAllImpls (envExpr : Expr) : TacticM $ List (Expr × Expr) := do
   let traitsListExpr ← mkAppM `Lampe.Env.traits #[envExpr]
-  let reducedTraitsListExpr ← withAtLeastTransparency .all <| whnf traitsListExpr
+  let reducedTraitsListExpr ← withAtLeastTransparency .all (whnf traitsListExpr)
 
-  let rec go (listExpr : Expr) : TacticM $ List (TSyntax `term) := do
-    dbg_trace "-------------------------------"
-    dbg_trace "RUNNING ON: listExpr {listExpr}"
-    dbg_trace "-------------------------------"
-
+  let rec go (listExpr : Expr) : TacticM $ List (Expr × Expr) := do
     let currentExpr ← withAtLeastTransparency .all <| whnf listExpr
+
     if currentExpr.isAppOfArity ``List.cons 3 then
-      dbg_trace "CONS CASE"
       let args := currentExpr.getAppArgs
-      let headExpr ← whnf args[1]! -- head
-      let tailExpr := args[2]! -- tail
-      if headExpr.isAppOfArity ``Prod.mk 4 then
-        dbg_trace "EXTRACTING : {headExpr}"
-        let implSyntax ← extractImpl headExpr
-        let restImpls ← go tailExpr
-        return implSyntax :: restImpls
-      else
-        throwError s!"extractAllImpls: expected Prod.mk in List.cons, got {headExpr}"
+      let headExpr ← whnf args[1]!
+      let tailExpr := args[2]!
+      let_expr Prod.mk _ _ name impl := headExpr
+        | throwError m!"unable to extract {← ppExpr headExpr} from environment {← ppExpr envExpr}"
+      let restImpls ← go tailExpr
+      return (name, impl) :: restImpls
+
     else if currentExpr.isAppOfArity ``List.nil 1 then
-      dbg_trace "NIL CASE"
       return []
+
     else if currentExpr.isAppOfArity ``List.append 3 then
-      dbg_trace "APPEND CASE"
       let args := currentExpr.getAppArgs
-      let list1Expr := args[1]! -- first list
-      let list2Expr := args[2]! -- second list
+      let list1Expr := args[1]!
+      let list2Expr := args[2]!
       let impls1 ← go list1Expr
       let impls2 ← go list2Expr
       return impls1 ++ impls2
-    else if currentExpr.isSorry then
-      dbg_trace s!"extractAllImpls: encountered sorry at {currentExpr}, treating as empty list part."
-      return []
+
     else
-      throwError s!"extractAllImpls: unhandled expression form {currentExpr}"
+      throwError m!"unable to match on {← ppExpr currentExpr} in environment {← ppExpr envExpr}"
 
   go reducedTraitsListExpr
-
-def trait1 : Lampe.TraitImpl := sorry
-def trait2 : Lampe.TraitImpl := sorry
-def trait3 : Lampe.TraitImpl := sorry
-def trait4 : Lampe.TraitImpl := sorry
-def trait5 : Lampe.TraitImpl := sorry
-def trait6 : Lampe.TraitImpl := sorry
-def trait7 : String × Lampe.TraitImpl := ("Trait7", trait6)
-
-
-def env1 : Lampe.Env := ⟨[], [("Trait1", trait1), ("Trait2", trait2)]⟩
-def env2 : Lampe.Env := ⟨[], [("Trait3", trait3), ("Trait4", trait4)]⟩
-def env3 : Lampe.Env := ⟨[], [trait7]⟩
-def env4 : Lampe.Env := ⟨[], [("Trait5", trait5)]⟩
-def env5 := env4
-def env6 := env5
-
-def env7 := env1 ++ env2 ++ env3 ++ env6
-
-elab "testing" env:term : tactic => do
-  let xs ← extractAllImpls (← elabTerm env none)
-  dbg_trace "ALL OF THE REST: {← xs.mapM $ (elabTerm · none)}"
-  return ()
-
-example : Prop := by
-  testing env7
-  sorry
 
 open Lampe in
 theorem callTrait_direct_intro {impls : List $ Lampe.Ident × Function}
@@ -99,6 +62,15 @@ theorem callTrait_direct_intro {impls : List $ Lampe.Ident × Function}
   · assumption
   · assumption
 
+
+/--
+Tactic to make progress on goals of the form `Expr.call (FuncRef.trait ...)` by trying all of the
+trait implementations in a given environment
+
+Usage: `try_all_traits <generics> <environment>`
+* generics: A list of values to instantiate the generic type parameters
+* environment: The environment to search the trait implementation
+-/
 elab "try_all_traits" "[" generics:term,* "]" env:term : tactic => do
   let envExpr ← elabTerm env none
   let impls ← extractAllImpls envExpr
@@ -106,53 +78,58 @@ elab "try_all_traits" "[" generics:term,* "]" env:term : tactic => do
   let oldState ← saveState
   let mainGoal ← getMainGoal
 
-  for impl in impls do
-    let implExpr ← elabTerm impl none
-    let implExprImpls ← whnf (← mkAppM `Lampe.TraitImpl.impl #[implExpr])
-    let funcList ← (withAtLeastTransparency .all (whnf (mkApp implExprImpls (← elabTerm generics
-    none))))
+  for (name, impl) in impls do
+    trace[Lampe.Traits] m!"attempting trait {← ppExpr name}"
 
-    let (_, funcs) ← liftOption funcList.listLit?
-    dbg_trace "trying impl"
+    let implExprImpls ← mkAppM `Lampe.TraitImpl.impl #[impl]
+    let funcList ←
+      (withAtLeastTransparency .all (whnf (mkApp implExprImpls (← elabTerm generics none))))
+
+    let some (_, funcs) := funcList.listLit? |
+      throwError m!"unable to match on {← ppExpr funcList} in trait implementation {← ppExpr impl}"
 
     for func in funcs do
-      dbg_trace "trying func"
       let funcArgs := func.getAppArgs
       let funcFunc := funcArgs.reverse[0]!
+      let funcName := funcArgs.reverse[1]!
 
+      trace[Lampe.Traits] m!"  attempting function {← ppExpr funcName}"
+      -- In order for the metavariables to unify in the right way, we need to run these
+      -- tactics in this order as otherwise `tauto` is overly eager to fill in data
+      -- incorrectly
       try
-        dbg_trace "1: {(← getUnsolvedGoals).length}"
+        -- `callTrait_direct_intro` on the main goal
         let callDirectGoals ← evalTacticAt (←`(tactic|
           apply callTrait_direct_intro (func := $(←funcFunc.toSyntax))
         )) mainGoal
 
-        dbg_trace "2: {(← getUnsolvedGoals).length}"
         let traitResGoal := callDirectGoals[0]!
+         -- start filling in the `TraitResolution` goal
         let traitResGoals ← evalTacticAt (←`(tactic|
-          apply Lampe.TraitResolution.ok (impl := $impl) (implGenerics := $generics) (h_mem := by tauto)
+          apply Lampe.TraitResolution.ok (impl := $(← impl.toSyntax)) (implGenerics := $generics) (h_mem := by tauto)
         )) traitResGoal
 
-        dbg_trace "3: {(← getUnsolvedGoals).length}"
+        -- solve the `callTrait_direct_intro` goals first
         for goal in callDirectGoals.drop 1 do
           let newGoals ← evalTacticAt (← `(tactic|
             first | try tauto | try with_unfolding_all rfl
           )) goal
           pushGoals newGoals
 
-        dbg_trace "4: {(← getUnsolvedGoals).length}"
+        -- finally solve the `TraitResolution` goals
         for goal in traitResGoals do
           let goals ← evalTacticAt (←`(tactic| first | try tauto | with_unfolding_all rfl)) goal
           pushGoals goals
 
-        dbg_trace "5: {(← getUnsolvedGoals).length}"
         if (← getUnsolvedGoals).length == 1 then
-          dbg_trace "Success!"
+          -- one last `simp only` call to clean up the goal state
+          evalTactic (← `(tactic| simp only))
           return
         else
           oldState.restore
-          dbg_trace "unsolved: {(← getUnsolvedGoals).length}"
       catch _ =>
         oldState.restore
 
   oldState.restore
-  throwError "No trait implementation could be applied successfully"
+  throwError m!"no matching trait implementation found in environment {← ppExpr envExpr}"
+
