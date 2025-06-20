6a8fd7c9a2716784544c4274f604b5fd7950c478
Ara Adkins <me@ara.io>
2025-06-20 09:47:26 +0200

    Work on proving monotonicity for Omni

--- Lampe/Lampe/Monotonicity.lean
+++ Lampe/Lampe/Monotonicity.lean
@@ -7,38 +7,6 @@ import Lampe.Tp
 
 namespace Lampe
 
-namespace STHoare
-
--- States that a given theorem on a Hoare Triple is valid for any environment Γₒ that contains the
--- environment Γᵢ for which the theorem was originally proven.
---
--- In detail:
---
--- - `p` is the value of the field prime under which the proof should hold.
--- - `Γᵢ` is the "inner" environment, namely the one for which a proof of the Hoare triple already
---   exists, while `Γₒ` is the "outer" environment, the one for which we want our existing proof to
---   hold.
--- - `pre` is the precondition for our Hoare triples, namely the state in which our program is
---   before executing `expr`.
--- - `expr` is the program expression to be "executed" in both cases.
--- - `post` is the postcondition for our Hoare triples, namely the state in which our program will
---   end up if `expr` evaluates.
---
--- See the documentation for `STHoare` for more detail.
-theorem is_monotonic 
-    {p : Prime} 
-    {Γ₁ Γ₂ : Env}
-    {pre : SLP (State p)}
-    {expr : Expr (Tp.denote p) tp}
-    {post : Tp.denote p tp → SLP (State p)}
-    {innerSubOuter : Γ₁ ⊆ Γ₂}
-  : STHoare p Γ₁ pre expr post → STHoare p Γ₂ pre expr post := by
-  -- Obviously this is by induction over Omni, but HOW
-  intro h
-  sorry
-
-end STHoare
-
 namespace Env
 
 @[simp]
@@ -109,5 +77,72 @@ lemma subset_right_subset_of_append {Γᵢ Γₗ Γᵣ : Env} : Γᵢ ⊆ Γᵣ 
     apply subset_transitive h this
 
 end Env
+
+namespace Omni
+
+-- States that our any theorem over our Omnisemantics that is valid for any environment Γ₂ that
+-- contains the environment Γ₁ for which the theorem was originally proven.
+--
+-- In detail:
+--
+-- - `p` is the value of the field prime under which the proof should hold.
+-- - `Γ₁` is the "inner" environment, namely the one for which a proof of the Hoare triple already
+--   exists, while `Γ₂` is the "outer" environment, the one for which we want our existing proof to
+--   hold.
+-- - `st` is the state of the program before execution of the provided `expr`.
+-- - `expr` is the program expression to be "executed" in both cases.
+-- - `Q` represents the postcondition for execution, where it is `some` after successful execution
+--   and `none` otherwise. It is a predicate to help represent the explicit non-determinism of our
+--   semantics. Our postcondition is now a _set_, modeled as a selector. The interpretation is that
+--   we end up in `Q` no matter who controls the non-determinism.
+--
+-- See the documentation for `Omni` for more detail.
+theorem is_monotonic
+    {p : Prime}
+    {Γ₁ Γ₂ : Env}
+    {st : State p}
+    {expr : Expr (Tp.denote p) tp}
+    {Q : Option (State p × Tp.denote p tp) → Prop}
+    {innerSubOuter : Γ₁ ⊆ Γ₂}
+  : Omni p Γ₁ st expr Q → Omni p Γ₂ st expr Q := by
+  intro h
+  -- induction h
+  -- . 
+  
+  sorry
+
+end Omni
+
+namespace STHoare
+
+-- States that a given theorem on a Hoare Triple is valid for any environment Γ₂ that contains the
+-- environment Γ₁ for which the theorem was originally proven.
+--
+-- In detail:
+--
+-- - `p` is the value of the field prime under which the proof should hold.
+-- - `Γ₁` is the "inner" environment, namely the one for which a proof of the Hoare triple already
+--   exists, while `Γ₂` is the "outer" environment, the one for which we want our existing proof to
+--   hold.
+-- - `pre` is the precondition for our Hoare triples, namely the state in which our program is
+--   before executing `expr`.
+-- - `expr` is the program expression to be "executed" in both cases.
+-- - `post` is the postcondition for our Hoare triples, namely the state in which our program will
+--   end up if `expr` evaluates.
+--
+-- See the documentation for `STHoare` for more detail.
+theorem is_monotonic 
+    {p : Prime} 
+    {Γ₁ Γ₂ : Env}
+    {pre : SLP (State p)}
+    {expr : Expr (Tp.denote p) tp}
+    {post : Tp.denote p tp → SLP (State p)}
+    {innerSubOuter : Γ₁ ⊆ Γ₂}
+  : STHoare p Γ₁ pre expr post → STHoare p Γ₂ pre expr post := by
+  -- Obviously this is by induction over Omni, but HOW
+  intro h
+  sorry
+
+end STHoare
 
 end Lampe
