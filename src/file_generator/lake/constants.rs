8dafaa348ea2ccf968d7dd152f5bcc7ff3072911
Ara Adkins <iamrecursion@users.noreply.github.com>
2025-08-27 13:58:08 +0200

    Add the stdlib as a project dependency (#184)
    Please note that the current location and inclusion convention may
    change, as it is not final. It is unconditionally included as a git
    dependency in any newly-extracted project unless that project is the
    standard library itself.
    
    It is included with its current `name-version` string, as should be true
    for any extracted dependencies.

---
+++ src/file_generator/lake/constants.rs
@@ -0,0 +1,4 @@
+//! Contains useful constants for working with lake projects.
+
+/// The name of the noir standard library Nargo package, as set in `Nargo.toml`.
+pub const NOIR_STDLIB_PACKAGE_NAME: &str = "std";
