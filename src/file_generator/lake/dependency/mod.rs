97292573bd88e06256eaefa9be4e9aad8a41edc2
Matej Penciak <matej.penciak@gmail.com>
2025-09-03 16:04:33 -0400

    fix generated file structure

--- src/file_generator/lake/dependency/mod.rs
+++ src/file_generator/lake/dependency/mod.rs
@@ -16,6 +16,11 @@ pub use reservoir::LeanDependencyReservoir;
 #[allow(unused_imports)]
 pub use reservoir_git::LeanDependencyReservoirGit;
 
+use crate::{
+    file_generator::{self, NoirPackageIdentifier},
+    lean::{LEAN_QUOTE_END, LEAN_QUOTE_START},
+};
+
 pub trait LeanDependency {
     /// Generates the lean dependency.
     ///
@@ -23,4 +28,33 @@ pub trait LeanDependency {
     ///
     /// - If the dependency cannot be generated.
     fn generate(&self) -> Result<String, fmt::Error>;
+
+    fn name(&self) -> &str;
+
+    /// This is the best effort way to generate a NoirPackageIdentifier from a
+    /// name assuming everything is extracted as expected by Lampe
+    fn noir_package_identifier(&self) -> Result<NoirPackageIdentifier, file_generator::Error> {
+        // let stripped_name = self.name().strip_prefix(LEAN_QUOTE_START).ok_or(
+        //     file_generator::Error::LakeRequireGeneration(
+        //         "Lean dependency was in an unexpected form".to_string(),
+        //     ),
+        // )?;
+        // let stripped_name = stripped_name.strip_suffix(LEAN_QUOTE_END).ok_or(
+        //     file_generator::Error::LakeRequireGeneration(
+        //         "Lean dependency was in an unexpected form".to_string(),
+        //     ),
+        // )?;
+
+        let (name, version) =
+            self.name()
+                .split_once("-")
+                .ok_or(file_generator::Error::LakeRequireGeneration(
+                    "Lean dependency was in an unexpected form".to_string(),
+                ))?;
+
+        Ok(NoirPackageIdentifier {
+            name:    name.to_string(),
+            version: version.to_string(),
+        })
+    }
 }
--- src/file_generator/lake/dependency/path.rs
+++ src/file_generator/lake/dependency/path.rs
@@ -1,6 +1,6 @@
 use std::{fmt, fmt::Write};
 
-use crate::file_generator::lake::dependency::LeanDependency;
+use crate::file_generator::{lake::dependency::LeanDependency};
 
 /// This is Lean's local dependency.
 #[derive(Debug, Clone)]
@@ -61,5 +61,9 @@ impl LeanDependency for LeanDependencyPath {
         }
 
         Ok(result)
+    }
+
+    fn name(&self) -> &str {
+        &self.name
     }
 }
