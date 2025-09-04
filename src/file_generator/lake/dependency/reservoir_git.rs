6f32d7f4e6b9dd18362d85c9f606fc4f6f428f73
Ara Adkins <me@ara.io>
2025-09-04 14:04:53 +0200

    First pass at fixing names in the extractor

--- src/file_generator/lake/dependency/reservoir_git.rs
+++ src/file_generator/lake/dependency/reservoir_git.rs
@@ -1,6 +1,6 @@
 use std::{fmt, fmt::Write};
 
-use crate::file_generator::{lake::dependency::LeanDependency};
+use crate::file_generator::lake::dependency::LeanDependency;
 
 /// This is Lean's dependency from Reservoir (Lean's package manager) with git
 /// rev.
