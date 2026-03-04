1522463b48dfdb1c52262e957ba942e75406de88
krushimir <189111540+krushimir@users.noreply.github.com>
2026-03-04 12:09:00 +0100

    feat: prove `Hash.Mod` for tuples, containers, and `BuildHasherDefault`

--- stdlib/lampe/Stdlib/Hash/Mod.lean
+++ stdlib/lampe/Stdlib/Hash/Mod.lean
@@ -183,4 +183,145 @@ theorem i64_hash_spec {p H self state}
   · exact h_write _
   · intro; steps
 
+theorem tuple2_hash_spec {p A B H self state}
+    {A_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] A}
+    {B_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] B}
+    (h_hash_a : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] A h![] h![H] h![self.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_b : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] B h![] h![H] h![self.2.1, state])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.tuple none [A, B]) h![] h![H] h![self, state])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps [h_hash_a, h_hash_b]
+
+theorem tuple3_hash_spec {p A B C H self state}
+    {A_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] A}
+    {B_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] B}
+    {C_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] C}
+    (h_hash_a : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] A h![] h![H] h![self.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_b : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] B h![] h![H] h![self.2.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_c : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] C h![] h![H] h![self.2.2.1, state])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.tuple none [A, B, C]) h![] h![H] h![self, state])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps [h_hash_a, h_hash_b, h_hash_c]
+
+theorem tuple4_hash_spec {p A B C D H self state}
+    {A_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] A}
+    {B_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] B}
+    {C_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] C}
+    {D_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] D}
+    (h_hash_a : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] A h![] h![H] h![self.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_b : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] B h![] h![H] h![self.2.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_c : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] C h![] h![H] h![self.2.2.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_d : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] D h![] h![H] h![self.2.2.2.1, state])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.tuple none [A, B, C, D]) h![] h![H] h![self, state])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps [h_hash_a, h_hash_b, h_hash_c, h_hash_d]
+
+theorem tuple5_hash_spec {p A B C D E H self state}
+    {A_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] A}
+    {B_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] B}
+    {C_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] C}
+    {D_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] D}
+    {E_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] E}
+    (h_hash_a : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] A h![] h![H] h![self.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_b : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] B h![] h![H] h![self.2.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_c : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] C h![] h![H] h![self.2.2.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_d : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] D h![] h![H] h![self.2.2.2.1, state])
+      (fun _ => ⟦⟧))
+    (h_hash_e : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] E h![] h![H] h![self.2.2.2.2.1, state])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.tuple none [A, B, C, D, E]) h![] h![H] h![self, state])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps [h_hash_a, h_hash_b, h_hash_c, h_hash_d, h_hash_e]
+
+theorem build_hasher_default_spec {p H _self}
+    {H_hasher : «std-1.0.0-beta.12::hash::Hasher».hasImpl env h![] H}
+    {H_default : «std-1.0.0-beta.12::default::Default».hasImpl env h![] H}
+    (h_default : STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::default::Default».default h![] H h![] h![] h![])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::BuildHasher».build_hasher h![]
+        («std-1.0.0-beta.12::hash::BuildHasherDefault».tp h![H]) h![H] h![] h![_self])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps [h_default]
+
+theorem default_build_hasher_default_spec {p H}
+    {H_hasher : «std-1.0.0-beta.12::hash::Hasher».hasImpl env h![] H}
+    {H_default : «std-1.0.0-beta.12::default::Default».hasImpl env h![] H} :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::default::Default».default h![]
+        («std-1.0.0-beta.12::hash::BuildHasherDefault».tp h![H]) h![] h![] h![])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps
+
+theorem array_hash_spec {p N T H self state}
+    {T_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] T}
+    (h_hash_elem : ∀ (elem : Tp.denote p T), STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] T h![] h![H] h![elem, state])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.array T N) h![] h![H] h![self, state])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps
+  loop_inv nat (fun _ _ _ => ⟦⟧)
+  · simp
+  · intros; steps [h_hash_elem]
+  · steps
+
+theorem slice_hash_spec {p T H} {self : List (Tp.denote p T)} {state}
+    {T_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] T}
+    {u32_hash : «std-1.0.0-beta.12::hash::Hash».hasImpl env h![] (.u 32)}
+    (h_hash_len : ∀ (len : U 32), STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.u 32) h![] h![H] h![len, state])
+      (fun _ => ⟦⟧))
+    (h_hash_elem : ∀ (elem : Tp.denote p T), STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] T h![] h![H] h![elem, state])
+      (fun _ => ⟦⟧)) :
+    STHoare p env ⟦⟧
+      («std-1.0.0-beta.12::hash::Hash».hash h![] (.slice T) h![] h![H] h![self, state])
+      (fun _ => ⟦⟧) := by
+  resolve_trait
+  steps [h_hash_len]
+  loop_inv nat (fun _ _ _ => ⟦⟧)
+  · simp
+  · intros; steps [h_hash_elem]
+  · steps
+
 end Lampe.Stdlib.Hash.Mod
