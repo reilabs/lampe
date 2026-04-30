609907cbae5e257200a1c90dce31c74fe44e90af
Akosh Farkash <aakoshh@gmail.com>
2026-04-30 21:57:11 +0100

    Use nargo error reporting to show file locations (#281)

--- src/noir/error/compilation.rs
+++ src/noir/error/compilation.rs
@@ -1,7 +1,7 @@
 //! Errors to do with compilation of Noir.
 
 use noirc_driver::ErrorsAndWarnings;
-use noirc_frontend::hir::def_collector::dc_crate::CompilationError;
+use noirc_errors::reporter::ReportedErrors;
 use thiserror::Error;
 
 /// The result type for dealing with compilation-related errors.
@@ -13,8 +13,8 @@ pub enum Error {
     #[error("Could not successfully check crate:\n{diagnostics:?}")]
     CheckFailure { diagnostics: ErrorsAndWarnings },
 
-    #[error("Could not successfully compile crate:\n{errors:?}")]
-    CompileFailure { errors: Vec<CompilationError> },
+    #[error("Could not successfully compile crate; reported {} errors", _0.error_count)]
+    Reported(ReportedErrors),
 
     #[error("Noir Error: {_0}")]
     Other(String),
